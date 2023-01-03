35f859ead1e34e3032604ca5acf8cabe69378947
Rafael Oliveira <rafael.oliveira@gatec.com.br>
2023-01-03 12:37:37 -0300

    test server

--- index.js
+++ index.js
@@ -19,10 +19,14 @@ const client = new Client({
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 
+    /*
+        WOK Documentation
+        https://docs.wornoffkeys.com
+    */
     new WOK({
         client,
         commandsDir: path.join(__dirname, 'commands'),
-        testServers: ['986608378233098320'],
+        testServers: ['986608378233098320', '295732999058882572'],
         disabledDefaultCommands: [
             DefaultCommands.ChannelCommand,
             DefaultCommands.CustomCommand,
